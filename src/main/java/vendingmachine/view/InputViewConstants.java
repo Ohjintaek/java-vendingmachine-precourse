f9fcf3bcb5613c90addf68d6b27bee8b0acd720f
OhJintaek <sunshot@hanyang.ac.kr>
2022-12-09 16:40:56 +0900

    feat: 입력을 받을 때 필요한 상수 String 추가

---
+++ src/main/java/vendingmachine/view/InputViewConstants.java
@@ -0,0 +1,8 @@
+package vendingmachine.view;
+
+public class InputViewConstants {
+    public final String getVendingMachineMoneyMessage = "자판기가 보유하고 있는 금액을 입력해 주세요.";
+    public final String getProductMessage = "상품명과 가격, 수량을 입력해 주세요.";
+    public final String getMoneyMessage = "투입 금액을 입력해 주세요.";
+    public final String getProductNameMessage = "구매할 상품명을 입력해 주세요.";
+}
