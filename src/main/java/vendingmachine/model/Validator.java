f93f80f896f74f1364730d46a3d59f26cb16f5c1
OhJintaek <sunshot@hanyang.ac.kr>
2022-12-13 00:31:39 +0900

    feat: 10으로 나누어떨어지는 숫자인지 판단하는 기능 추가

--- src/main/java/vendingmachine/model/Validator.java
+++ src/main/java/vendingmachine/model/Validator.java
@@ -10,10 +10,16 @@ public class Validator {
             throw new IllegalArgumentException(error + "숫자를 입력해주세요.");
         }
     }
-    
+
     public static void isStartZero(String userInput) {
         if (userInput.startsWith("0")) {
             throw new IllegalArgumentException(error + "0으로 시작하지 않는 숫자를 입력해주세요.");
+        }
+    }
+
+    public static void isDividableTen(int number) {
+        if (number % 10 != 0) {
+            throw new IllegalArgumentException(error + "10으로 나누어지는 숫자를 입력해주세요.");
         }
     }
 }
