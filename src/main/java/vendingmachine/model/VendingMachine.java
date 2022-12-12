1d8dfafdf2dc6c1ef3cb66612532041875e53db4
OhJintaek <sunshot@hanyang.ac.kr>
2022-12-12 20:08:43 +0900

    자판기 보유 금액이 입력되었을 때 VendingMachine 클래스를 생성하도록 기능 추가

--- src/main/java/vendingmachine/model/VendingMachine.java
+++ src/main/java/vendingmachine/model/VendingMachine.java
@@ -7,8 +7,14 @@ import java.util.HashMap;
 import java.util.List;
 
 public class VendingMachine {
+    private int money;
     private List<Product> products = new ArrayList<>();
     private HashMap<Coin, Integer> coins = new HashMap<>();
+
+    VendingMachine(int money) {
+        this.money = money;
+        initCoins();
+    }
 
     private void initCoins() {
         coins.put(Coin.COIN_500, 0);
