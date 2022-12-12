9d711ead27331915581652eb28b2637152dfee32
OhJintaek <sunshot@hanyang.ac.kr>
2022-12-12 21:53:02 +0900

    feat: 물품이 구매되었을 때 재고가 줄어드는 기능 추가

--- src/main/java/vendingmachine/model/Product.java
+++ src/main/java/vendingmachine/model/Product.java
@@ -1,13 +1,18 @@
 package vendingmachine.model;
 
 public class Product {
-    private String name;
-    private int price;
+    private final String name;
+    private final int price;
     private int stock;
 
     public Product(String name, int price, int stock) {
         this.name = name;
         this.price = price;
         this.stock = stock;
+    }
+
+    public void buyThisProduct() {
+        stock--;
+        //구매할 수 없는 경우 예외처리
     }
 }
