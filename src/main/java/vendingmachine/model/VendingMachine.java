bdb22cccee8ef1736bc8272e3dccbd46a868f3a9
OhJintaek <sunshot@hanyang.ac.kr>
2022-12-12 20:05:51 +0900

    feat: 자판기가 보유한 잔돈을 HashMap형태로 저장

--- src/main/java/vendingmachine/model/VendingMachine.java
+++ src/main/java/vendingmachine/model/VendingMachine.java
@@ -1,10 +1,19 @@
 package vendingmachine.model;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class VendingMachine {
-    private int money;
     private List<Product> products = new ArrayList<>();
-    private List<Coin> coins = new ArrayList<>();
+    private HashMap<Coin, Integer> coins = new HashMap<>();
+
+    private void initCoins() {
+        coins.put(Coin.COIN_500, 0);
+        coins.put(Coin.COIN_100, 0);
+        coins.put(Coin.COIN_50, 0);
+        coins.put(Coin.COIN_10, 0);
+    }
 }
