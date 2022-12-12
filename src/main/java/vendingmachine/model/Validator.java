9419bfc0cb9b88020cd4dfaae2a162e7b4818eaa
OhJintaek <sunshot@hanyang.ac.kr>
2022-12-13 00:44:22 +0900

    feat: 올바른 형식의 상품을 입력했는지 판단하는 기능 추가

--- src/main/java/vendingmachine/model/Validator.java
+++ src/main/java/vendingmachine/model/Validator.java
@@ -1,7 +1,11 @@
 package vendingmachine.model;
 
+import java.util.regex.Pattern;
+
 public class Validator {
     public static final String error = "[Error] ";
+    public static final String format = "^\\[[a-zA-Z0-9가-힣]+,\\d+,\\d+]$";
+
     public static int isValidNumber(String userInput) {
         isStartZero(userInput);
         try {
@@ -20,6 +24,12 @@ public class Validator {
     public static void isDividableTen(int number) {
         if (number % 10 != 0) {
             throw new IllegalArgumentException(error + "10으로 나누어지는 숫자를 입력해주세요.");
+        }
+    }
+
+    public static void isValidProduct(String product) {
+        if (!Pattern.matches(format, product)) {
+            throw new IllegalArgumentException(error + "잘못된 형식의 상품명을 입력했습니다.");
         }
     }
 }
