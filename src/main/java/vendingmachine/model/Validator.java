94095630ea13f2da5bd6f085d811d37d0ddef5a4
OhJintaek <sunshot@hanyang.ac.kr>
2022-12-13 00:29:54 +0900

    feat: 올바른 숫자가 입력됐는지 판단하는 기능 추가

---
+++ src/main/java/vendingmachine/model/Validator.java
@@ -0,0 +1,19 @@
+package vendingmachine.model;
+
+public class Validator {
+    public static final String error = "[Error] ";
+    public static int isValidNumber(String userInput) {
+        isStartZero(userInput);
+        try {
+            return Integer.parseInt(userInput);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(error + "숫자를 입력해주세요.");
+        }
+    }
+    
+    public static void isStartZero(String userInput) {
+        if (userInput.startsWith("0")) {
+            throw new IllegalArgumentException(error + "0으로 시작하지 않는 숫자를 입력해주세요.");
+        }
+    }
+}
