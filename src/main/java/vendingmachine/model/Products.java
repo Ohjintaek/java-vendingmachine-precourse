1db98f3316859d433a7d41ffcf2989eefa00157c
OhJintaek <sunshot@hanyang.ac.kr>
2022-12-13 13:32:33 +0900

    refactor: 상품정보 문자열 분리를 Products 클래스에서 하도록 수정

--- src/main/java/vendingmachine/model/Products.java
+++ src/main/java/vendingmachine/model/Products.java
@@ -1,12 +1,17 @@
 package vendingmachine.model;
 
 import java.util.HashMap;
+import java.util.List;
 
 public class Products {
     private HashMap<String, Product> products = new HashMap<>();
     private int minPrice = Integer.MAX_VALUE;
 
-    public void addProduct(String name, int price, int stock) {
+    public void addProduct(String product) {
+        String[] productDetail = product.split(",");
+        String name = productDetail[0];
+        int price = Integer.parseInt(productDetail[1]);
+        int stock = Integer.parseInt(productDetail[2]);
         Product newProduct = new Product(name, price, stock);
         products.put(name, newProduct);
         minPrice = Math.min(minPrice, price);
